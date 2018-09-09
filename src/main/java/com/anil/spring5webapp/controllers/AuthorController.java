7c4d3bd0de62339f0225d7e671d4edbe7b94f2c3
Anil_Ramesh <anil.hhh.desai@gmail.com>
2018-09-09 20:48:06 +0100

    Thymeleaf added for Authors

---
+++ src/main/java/com/anil/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.anil.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.anil.spring5webapp.repositories.AuthorRepository;
+
+/**
+ * @author Anil_Ramesh
+ *
+ */
+@Controller
+public class AuthorController {
+
+	private AuthorRepository authorRepository;
+
+	public AuthorController(AuthorRepository authorRepository) {
+		super();
+		this.authorRepository = authorRepository;
+	}
+
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+
+		model.addAttribute("authors", authorRepository.findAll());
+		return "authors";
+	}
+
+}
